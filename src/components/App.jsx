16b74b561bdf2c074131591d210b3523b236f808
Mariano <51141999+mariano-allevato@users.noreply.github.com>
2025-04-01 08:52:41 -0300

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+  // App.jsx
+  import VueloInfo from './components/VueloInfo';
+
+  export default function App() {
+    return (
+      <div className="app-container">
+        <h1>Example Lite</h1>
+        <VueloInfo />
+      </div>
+    );
+  }
